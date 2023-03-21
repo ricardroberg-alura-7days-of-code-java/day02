48d48df7d7d15bf3d0994fdba41e2e5a83ef78f6
Ricard Roberg <ricardroberg@gmail.com>
2023-03-21 09:15:20 -0300

    Codigo finalizado. Saída um pouco diferente mas seguindo os mesmos princípios.

--- src/main/java/ListaFilmesIMDB.java
+++ src/main/java/ListaFilmesIMDB.java
@@ -1,2 +1,52 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class ListaFilmesIMDB {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Uma pequena ajuda do CHATGPT
+        Dotenv dotenv = Dotenv.load();
+        String api_key = dotenv.get("APIKEY");
+        String url = "https://imdb-api.com/en/API/Top250TVs/" + api_key;
+
+        URL imdbApiUrl = new URL(url);
+
+        HttpURLConnection con = (HttpURLConnection) imdbApiUrl.openConnection();
+        con.setRequestMethod("GET");
+
+        con.setRequestProperty("User-Agent", "My Java Application");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        // Convertendo de volta para JSON pois não consegui iterar sobre StringBuilder
+        JSONObject json = new JSONObject(response.toString());
+        JSONArray results = json.getJSONArray("items");
+
+        for (int i = 0; i < 250; i++) {
+            JSONObject result = results.getJSONObject(i);
+            String title = result.getString("title");
+            String image = result.getString("image");
+
+            System.out.println("TITLE: " + title);
+            System.out.println("IMAGEM: " + image);
+
+        }
+    }
 }
+
